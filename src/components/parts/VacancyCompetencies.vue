f77260e10838f02a7bb981b4c43e62917292b07a
Silweron <silwerlerman.yandex.ru>
2021-06-10 18:03:16 +0300

    Сделан блок "Ключевые слова" основной страницы

---
+++ src/components/parts/VacancyCompetencies.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="vacancy-competencies">
+    <VacancyDevider deviderSize="24" />
+    <h3 class="competencies__title">Ключевые навыки</h3>
+    <VacancyDevider deviderSize="12" />
+    <div class="header__tags">
+      <el-tag class="tags__item" type="info">CMS на PHP</el-tag>
+      <el-tag class="tags__item" type="info">GIT</el-tag>
+    </div>
+  </div>
+</template>
+
+<script>
+import VacancyDevider from "@/components/elements/VacancyDevider";
+export default {
+  name: "vacancy-page",
+  components: { VacancyDevider },
+};
+</script>
+
+<style>
+.competencies__title {
+  margin: 0px;
+}
+.vacancy-competencies {
+  max-width: 530px;
+}
+.vacancy-header {
+  display: flex;
+  flex-direction: column;
+}
+.header__tags {
+  display: flex;
+  flex-direction: row;
+  max-width: 530px;
+  flex-wrap: wrap;
+}
+.tags__item {
+  margin-right: 8px;
+}
+</style>
--- src/components/parts/VacancyPage.vue
+++ src/components/parts/VacancyPage.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="vacancy-page">
     <VacancyHeader />
-    <VacancyDevider deviderSize="24" hasLine="true" />
+    <VacancyDevider deviderSize="24" hasLine />
+    <div class="vacancy-page__competencies flex-between">
+      <VacancyCompetencies />
+      <p class="competencies__update">Последние изменения: {{ lastUpdate }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import VacancyHeader from "./VacancyHeader";
 import VacancyDevider from "@/components/elements/VacancyDevider";
+import VacancyCompetencies from "./VacancyCompetencies";
 export default {
+  data() {
+    return {
+      lastUpdate: "25.05.2021",
+    };
+  },
   name: "vacancy-page",
-  components: { VacancyHeader, VacancyDevider },
+  components: { VacancyHeader, VacancyDevider, VacancyCompetencies },
 };
 </script>
 
 <style>
+.competencies__update {
+  padding-top: 25px;
+  margin: 0px 0px 0px -70px;
+  color: #9e9e9e;
+  min-width: 257px;
+  max-height: 21px;
+}
 .vacancy-page {
   background: #ffffff;
   border-radius: 12px;
@@ -22,5 +39,12 @@ export default {
   margin: 20px;
   max-width: 720px;
   word-wrap: break-word;
+}
+.vacancy-page__competencies {
+  display: flex;
+  flex-direction: row;
+}
+.flex-between {
+  justify-content: space-between;
 }
 </style>
